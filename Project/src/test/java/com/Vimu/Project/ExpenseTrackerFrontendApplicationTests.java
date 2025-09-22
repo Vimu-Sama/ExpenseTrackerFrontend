1762f8e955584a44810e0d8c8a39e73bd383cec9
Vimarsh Sharma <vimarshsharmabsr2000@gmail.com>
2025-09-22 11:48:33 +0530

    Project intialisation

---
+++ Project/src/test/java/com/Vimu/Project/ExpenseTrackerFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Vimu.Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseTrackerFrontendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
